17f52370d82cd679a25c0e3752bdaac1864684ab
DraX <51809574+DrXag@users.noreply.github.com>
2020-01-06 23:17:36 +0530

    Add files via upload

---
+++ My Dictionary.py
@@ -0,0 +1,11 @@
+Dic={"Me":"DraX",
+    "Drax":"DrXag",
+    "DrXag":"Died",
+    "Died":"Noone",
+    "Noone":"No",
+    "No":"Who care",
+    "Who care":"Fuck off",
+    "Fuck off":"Fuck you"}
+print("What can i do for you???")
+inp=input()
+print(Dic[inp])
